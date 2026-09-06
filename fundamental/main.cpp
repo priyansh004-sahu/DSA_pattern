b0ff623a0a6fe004195ecad0dd58538beb92a8b2
priyansh sahu <sahupriyansh298@gmail.com>
2026-09-06 20:27:03 +0530

    improved readability and maintainability

---
+++ fundamental/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+//    cout << "enter three number " << endl;
+//    int a, b, c;
+//    cin >> a >> b >> c;
+//    cout  << "multiplication of three number is " << a * b * c << endl;
+
+   int d,e;
+   cin >> d >> e;
+   cout << "average of two number is " << (d + e) / 2 << endl;
+}
